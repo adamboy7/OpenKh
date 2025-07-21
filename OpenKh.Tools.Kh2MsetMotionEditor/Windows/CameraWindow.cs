a1129654e6ae46210eda6f0d2339bdb896f5a4c5
adamboy7 <adamboy8888@gmail.com>
2025-07-21 12:41:45 -0400

    Fix camera controls and inspector

--- OpenKh.Tools.Kh2MsetMotionEditor/Windows/CameraWindow.cs
+++ OpenKh.Tools.Kh2MsetMotionEditor/Windows/CameraWindow.cs
@@ -10,9 +10,9 @@ namespace OpenKh.Tools.Kh2MsetMotionEditor.Windows
         {
             ForEdit3("Position", () => camera.CameraPosition, x => camera.CameraPosition = x);
             ForEdit2("Rotation",
-                () => new Vector2(-camera.CameraRotationYawPitchRoll.X, -camera.CameraRotationYawPitchRoll.Z),
+                () => new Vector2(-camera.CameraRotationYawPitchRoll.X, -camera.CameraRotationYawPitchRoll.Y),
                 x => camera.CameraRotationYawPitchRoll = new Vector3(
-                    -x.X, camera.CameraRotationYawPitchRoll.Y, -x.Y));
+                    -x.X, -x.Y, camera.CameraRotationYawPitchRoll.Z));
         });
     }
 }
